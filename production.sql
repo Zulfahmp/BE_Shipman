9acdb265919ee4586091448b1c536cdf165d0a52
vembririyan <vembrivrd@gmail.com>
2026-01-14 16:25:11 +0700

    update

---
+++ production.sql
@@ -0,0 +1,17 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : MyPostgre
+ Source Server Type    : PostgreSQL
+ Source Server Version : 170006 (170006)
+ Source Host           : localhost:5432
+ Source Catalog        : pertamina
+ Source Schema         : production
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 170006 (170006)
+ File Encoding         : 65001
+
+ Date: 14/01/2026 16:24:25
+*/
+
