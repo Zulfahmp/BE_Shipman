d0d047d38906d563bae7b113ab3021e3c3aded4c
ZulfaHidayat <zulfahidayat13@gmail.com>
2026-04-15 22:40:32 +0700

    updateAuth.js6

--- app/connection.js
+++ app/connection.js
@@ -1,22 +1,35 @@
 const { Pool } = require("pg");
 
-module.exports = {
-  DBP: () => {
-    try {
-      return new Pool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      });
-    } catch (err) {
-      console.error("DB connection error:", err);
-
-      throw err;
-    }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
+});
+
+module.exports = {
+  DBP: () => pool,
 };
+
+// const { Pool } = require("pg");
+
+// module.exports = {
+//   DBP: () => {
+//     try {
+//       return new Pool({
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_NAME,
+//         port: process.env.DB_PORT,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       });
+//     } catch (err) {
+//       console.error("DB connection error:", err);
+
+//       throw err;
+//     }
+//   },
+// };
