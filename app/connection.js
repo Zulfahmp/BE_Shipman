3f3cb68449056b7a11af6d4c8ac30ca414f55827
ZulfaHidayat <zulfahidayat13@gmail.com>
2026-04-15 23:05:55 +0700

    updateAuth.js10

--- app/connection.js
+++ app/connection.js
@@ -17,11 +17,11 @@ module.exports = {
   DBP: () => {
     try {
       return new Pool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        port: process.env.PGPORT,
         ssl: {
           rejectUnauthorized: false,
         },
